f610f14ff5c1ebc836f90a74fdef195c8fa887f2
Devan Becker <d7becker@gmail.com>
2026-09-03 10:46:04 -0400

    Replace "=" with "<-" for assignment

--- r_book.R
+++ r_book.R
@@ -1,10 +1,22 @@
 ## @knitr load_packages
 # Any package that is required by the script below is given here:
-inst_pkgs = load_pkgs =  c("ggplot2", "devtools", "zoo", 
-                           "plot3D", "lmtest", "faraway", "leaps", "broom", 
-                           "readr", "bestglm", "kernlab")
-inst_pkgs = inst_pkgs[!(inst_pkgs %in% installed.packages()[,"Package"])]
-if (length(inst_pkgs)) install.packages(inst_pkgs)
+inst_pkgs <- load_pkgs <- c(
+  "ggplot2",
+  "devtools",
+  "zoo",
+  "plot3D",
+  "lmtest",
+  "faraway",
+  "leaps",
+  "broom",
+  "readr",
+  "bestglm",
+  "kernlab"
+)
+inst_pkgs <- inst_pkgs[!(inst_pkgs %in% installed.packages()[, "Package"])]
+if (length(inst_pkgs)) {
+  install.packages(inst_pkgs)
+}
 
 # Dynamically load packages
-pkgs_loaded = lapply(load_pkgs, require, character.only = TRUE)
+pkgs_loaded <- lapply(load_pkgs, require, character.only = TRUE)
